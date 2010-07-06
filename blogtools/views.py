2c7f79dc998242fd622f76ee1cfce217d4723b36
Julien Phalip <jphalip@gmail.com>
2010-07-06 12:42:16 +1000

    Made entry_queryset and extra_context parameters passed through kwargs in views. This allows more flexibility for how parameters are passed and makes it cleaner if we need to pass more parameters in the future.

--- blogtools/views.py
+++ blogtools/views.py
@@ -19,9 +19,11 @@ class BaseEntryViews(object):
     template_root_path = None
     publication_date_field = 'pub_date'
 
-    def archive_index(self, request, entry_queryset=None, *args, **kwargs):
-        if entry_queryset is not None:
-            queryset = entry_queryset
+
+    def archive_index(self, request, *args, **kwargs):
+        if 'entry_queryset' in kwargs:
+            queryset = kwargs['entry_queryset']
+            del kwargs['entry_queryset']
         else:
             queryset = self.entry_queryset
         info_dict = {
@@ -31,9 +33,10 @@ class BaseEntryViews(object):
              }
         return list_detail.object_list(request, *args, **dict(info_dict, **kwargs))
 
-    def archive_year(self, request, entry_queryset=None, *args, **kwargs):
-        if entry_queryset is not None:
-            queryset = entry_queryset
+    def archive_year(self, request, *args, **kwargs):
+        if 'entry_queryset' in kwargs:
+            queryset = kwargs['entry_queryset']
+            del kwargs['entry_queryset']
         else:
             queryset = self.entry_queryset
         info_dict = {
@@ -44,9 +47,10 @@ class BaseEntryViews(object):
             }
         return date_based.archive_year(request, *args, **dict(info_dict, make_object_list=True, **kwargs))
 
-    def archive_month(self, request, entry_queryset=None, *args, **kwargs):
-        if entry_queryset is not None:
-            queryset = entry_queryset
+    def archive_month(self, request, *args, **kwargs):
+        if 'entry_queryset' in kwargs:
+            queryset = kwargs['entry_queryset']
+            del kwargs['entry_queryset']
         else:
             queryset = self.entry_queryset
         info_dict = {
@@ -56,10 +60,11 @@ class BaseEntryViews(object):
                 'template_object_name': 'entry',
             }
         return date_based.archive_month(request, *args, **dict(info_dict, month_format='%m', **kwargs))
-    
-    def archive_day(self, request, entry_queryset=None, *args, **kwargs):
-        if entry_queryset is not None:
-            queryset = entry_queryset
+
+    def archive_day(self, request, *args, **kwargs):
+        if 'entry_queryset' in kwargs:
+            queryset = kwargs['entry_queryset']
+            del kwargs['entry_queryset']
         else:
             queryset = self.entry_queryset
         info_dict = {
@@ -70,9 +75,10 @@ class BaseEntryViews(object):
             }
         return date_based.archive_day(request, *args, **dict(info_dict, month_format='%m', **kwargs))
 
-    def entry_detail(self, request, entry_queryset=None, *args, **kwargs):
-        if entry_queryset is not None:
-            queryset = entry_queryset
+    def entry_detail(self, request, *args, **kwargs):
+        if 'entry_queryset' in kwargs:
+            queryset = kwargs['entry_queryset']
+            del kwargs['entry_queryset']
         else:
             queryset = self.entry_queryset
         info_dict = {
@@ -82,69 +88,77 @@ class BaseEntryViews(object):
                 'template_object_name': 'entry',
             }
         return date_based.object_detail(request, *args, **dict(info_dict, month_format='%m', slug_field='slug', **kwargs))
-       
-    def search(self, request, entry_queryset=None, extra_context=None):
-        if entry_queryset is not None:
-            queryset = entry_queryset
+
+    def search(self, request, *args, **kwargs):
+        if 'entry_queryset' in kwargs:
+            queryset = kwargs['entry_queryset']
+            del kwargs['entry_queryset']
         else:
             queryset = self.entry_queryset
         query_string = ''
         found_entries = None
         if ('q' in request.GET) and request.GET['q'].strip():
             query_string = request.GET['q']
-            
+
             entry_query = get_query(query_string, ['title', 'body',])
-            
+
             found_entries = queryset.filter(entry_query)
-    
+
         context = {
             'query_string': query_string,
             'found_entries': found_entries
         }
-        context.update(extra_context or {})
-    
+        if 'extra_context' in kwargs:
+            context.update(kwargs['extra_context'] or {})
+
         return render_to_response('%s/search_results.html' % self.template_root_path,
                               context,
                               context_instance=RequestContext(request))
-    
-    def entry_preview(self, request, entry_pk, entry_queryset=None, extra_context=None):
-        if entry_queryset is not None:
-            queryset = entry_queryset
-        else:
-            queryset = self.entry_queryset
+
+    def entry_preview(self, request, entry_pk, *args, **kwargs):
         @staff_member_required
-        def func(request, entry_pk, extra_context=None):
+        def func(request, entry_pk, *args, **kwargs):
+            if 'entry_queryset' in kwargs:
+                queryset = kwargs['entry_queryset']
+                del kwargs['entry_queryset']
+            else:
+                queryset = self.entry_queryset
             context = {
                 'preview': True
             }
-            context.update(extra_context or {})
+            if 'extra_context' in kwargs:
+                context.update(kwargs['extra_context'] or {})
             return list_detail.object_detail(
-                     request, 
+                     request,
                      object_id=entry_pk,
                      queryset=queryset,
                      template_object_name='entry',
                      template_name='%s/entry_detail.html' % self.template_root_path,
                      extra_context=context
                  )
-        return func(request, entry_pk, extra_context)
+        return func(request, entry_pk, *args, **kwargs)
 
 try:
     from tagging.models import Tag
     from tagging.views import tagged_object_list
-    
+
     class TaggedEntryViewsMixin(object):
-    
-        def tag_list(self, request, extra_context=None):
+
+        def tag_list(self, request, *args, **kwargs):
+            extra_context = {}
+            if 'extra_context' in kwargs:
+                extra_context.update(kwargs['extra_context'] or {})
             return list_detail.object_list(
                { 'queryset': Tag.objects.all().order_by('name'),
                  'template_name': '%s/tag_list.html' % self.template_root_path,
                  'template_object_name': 'tag',
                  'extra_context': extra_context }
             )
-        
-        def tagged_entry_list(self, request, entry_queryset=None, *args, **kwargs):
-            if entry_queryset is not None:
-                queryset = entry_queryset
+
+        def tagged_entry_list(self, request, *args, **kwargs):
+            if 'entry_queryset' in kwargs:
+                queryset = kwargs['entry_queryset']
+                del kwargs['entry_queryset']
             else:
                 queryset = self.entry_queryset
             info_dict = {
@@ -153,7 +167,7 @@ try:
                  'template_object_name': 'entry',
                  }
             return tagged_object_list(request, *args, **dict(info_dict, **kwargs))
-        
+
         def json_tag_list(self, request):
             tags = [tag.name for tag in Tag.objects.all()]
             json = simplejson.dumps({ 'success': True, 'tags': tags })
